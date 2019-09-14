9a61efc609d11f90dc97664d07e4af32509a75b4
Cristiano Leopoldo Schroeder <cristianoleopoldo.schroeder@philips.com>
2019-09-14 13:15:19 -0300

    Check Logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,7 +45,7 @@ public class Program {
 			} 
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
-				sc.nextLine();
+				sc.nextLine(); 
 			}
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
